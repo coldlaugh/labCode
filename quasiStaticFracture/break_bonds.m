1534794d35e2d4993dc9a593c6771d053f68c7c8
Leyou <leyou@umich.edu>
2017-05-07 17:31:08 -0400

    changed break bond requirement

--- quasiStaticFracture/break_bonds.m
+++ quasiStaticFracture/break_bonds.m
@@ -1,18 +1,20 @@
-function [B,B_broken,G,relaxation]=break_bonds(X,X0,B,B_broken,S,G,W)
+function [B,B_broken,G,relaxation]=break_bonds(X,lRest,B,B_broken,S,G,W)
     
-    BC = @(x) [x(1)-round(x(1)/W)*W,x(2)];
+    
+    lCurrent = bondLength(X,B,W);
     r = zeros(size(B,1),1);
     for i = 1:size(B)
         n1 = B(i,1); n2 = B(i,2);
         if any(S==n1) || any(S==n2)
-            r(i) = norm(BC(X(n1,:) - X(n2,:))) - norm(BC(X0(n1,:) - X0(n2,:)));
+            r(i) = lCurrent(i) - lRest(i);
         else
             r(i) = 0;
         end
     end
 
-    [maxforce maxforcebond] = max(r./G);
-    relaxation = false;
+%     [maxforce maxforcebond] = max(r./G);
+    forceRatio = r ./ G ./rRest; % the ratio between the relative extension and the threshold
+    relaxation = any(forceRatio >= 1);
     if (maxforce > 1)
         B_broken = [B_broken;
             B(maxforcebond,:)];
